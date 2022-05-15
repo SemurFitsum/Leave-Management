cd71fd5d0121a849b3f3c9ba4a97e4af229432ae
Semur Fitsum <semur.fitsum@gmail.com>
2022-05-15 11:51:36 +0300

    Implemented Leave Allocation and Changed IdentityUser to Employee Added Leave Request Module for both Admin and Employee

--- Leave-Management/Startup.cs
+++ Leave-Management/Startup.cs
@@ -36,7 +36,7 @@ namespace Leave_Management
 
             //Add references for Repository and Contracts to Startup file
             services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
-            services.AddScoped<ILeaveHistoryRepository, LeaveHistoryRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
             services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
 
             // Auto Mapper
